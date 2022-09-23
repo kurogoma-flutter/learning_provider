45907a1f44f467ddd789ae5d136bf210bec8a5f1
Kurogoma939 <kan.sss.939@gmail.com>
2022-09-23 19:52:39 +0900

    メソッド・変数を管理するクラスを作成

---
+++ lib/counter_provider.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Counter extends ChangeNotifier {
+  //　状態管理をする数字
+  int counterA = 0;
+  int counterB = 0;
+  int counterC = 0;
+
+  /// counterAを増やす
+  void incrementA() {
+    counterA++;
+    notifyListeners();
+  }
+
+  /// counterBを増やす
+  void incrementB() {
+    counterB++;
+    notifyListeners();
+  }
+
+  /// counterCを増やす
+  void incrementC() {
+    counterC++;
+    notifyListeners();
+  }
+}
